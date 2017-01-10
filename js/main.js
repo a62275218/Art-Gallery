a75cbf47de69de6fab59bbeba4fc6d1f7500a22e
Jake <linrong123@qq.com>
2017-01-10 14:51:14 +1100

    index page

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Jake on 2017/1/8 0008.
+ */
+
+$('.carousel').carousel({
+    interval: 2000
+})
+
+$(function getWindowWidth() {
+    var width = $(document).body.offsetWidth;
+    return width;
+})
